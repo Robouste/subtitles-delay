1fdf82db6707cd4984524b36e13c98e4a400f0a6
Robin Denis <Robin.Denis@ucb.com>
2022-02-16 16:59:23 +0100

    - it's working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'subtitles-delay';
+	public subtitles: string = "";
+	public delay: number = 1000;
+	public output: string = "";
+
+	public process(): void {
+		this.output = this.subtitles.slice();
+
+		const regex: RegExp = /(\d{2}\:\d{2}\:\d{2}\.\d{3})/gm;
+		const matches: Set<string> = new Set();
+		let match: RegExpExecArray | null;
+
+		while ((match = regex.exec(this.output)) != null) {
+			matches.add(match[0]);
+		}
+
+		matches.forEach((time: string) => {
+			const date = new Date("1970-01-01 " + time.replace(".", ":"));
+			date.setMilliseconds(date.getMilliseconds() + this.delay);
+			console.log(time, date.getSeconds() + ":" + date.getMilliseconds());
+			const hour: string = date.getHours().toString().padStart(2, "0");
+			const min: string = date.getMinutes().toString().padStart(2, "0");
+			const sec: string = date.getSeconds().toString().padStart(2, "0");
+			const ms: string = date
+				.getMilliseconds()
+				.toString()
+				.padStart(3, "0");
+			const newTime = `${hour}:${min}:${sec}.${ms}`;
+
+			this.output = this.output.replaceAll(time, newTime);
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		CommonModule,
+		FormsModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatButtonModule,
+		MatInputModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
